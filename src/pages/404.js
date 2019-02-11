f42cc9d1adb38594f0850f1514ae80f7ef8677dc
osogrizz <lhtor@gmail.com>
2019-02-11 10:06:38 -0800

    Fixed 404 styles.

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-  <Layout style={{ textAlign: 'center' }}>
+  <Layout style={{ textAlign: 'center !important' }}>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
