d55b36562411631d30214a16d4c200983b9b15ea
osogrizz <lhtor@gmail.com>
2019-02-11 10:30:53 -0800

    ibid

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,10 @@ import SEO from '../components/seo'
 const Wrapper = styled.div`
   text-align: center;
   margin-top: 120px;
-  font-size: 5rem;
-  font-weight: 300;
+  h1 {
+    font-size: 5rem;
+    font-weight: 300;
+  }
 
 `
 
