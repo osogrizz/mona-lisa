aab26d21bf72ffea848344c9addc7fb065a943b3
osogrizz <lhtor@gmail.com>
2019-02-11 10:23:34 -0800

    Add styled components to 404.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+const Wrapper = styled.div`
+  text-align: center;
+`
+
 const NotFoundPage = () => (
-  <Layout style={{ textAlign: 'center !important' }}>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <Wrapper>
+    <Layout style={{ textAlign: 'center !important' }}>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  </Wrapper>   
 )
 
 export default NotFoundPage
