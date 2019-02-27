72dcb4254aad9cb0297af50011af4f795de4096d
osogrizz <lhtor@gmail.com>
2019-02-27 15:29:32 -0800

    Checking image behavior.

--- src/pages/index.js
+++ src/pages/index.js
@@ -175,9 +175,9 @@ export const PageQuery = graphql`
           thumbnail
           title
           author
-          desc
+          desc_
           price
-          IMDB
+          imdb
         }
       }
     }
