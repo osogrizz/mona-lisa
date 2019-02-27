dfcb501edb7296a720213f328e882e40a8f9635a
osogrizz <lhtor@gmail.com>
2019-02-26 16:44:11 -0800

    Added netlify plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,8 @@ module.exports = {
         icon: `src/images/flavicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
