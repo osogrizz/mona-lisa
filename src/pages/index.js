297427fc83c0f6b75d01f72a4fa41f1d0a80e6c1
osogrizz <lhtor@gmail.com>
2019-02-27 15:45:26 -0800

    Image widget deleting uploads.

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,13 +121,13 @@ class IndexPage extends Component {
         <CardContainer>
           {data.allMarkdownRemark.edges.map( ({node}) => (
             <Card key={node.id} item={node} >
-              {/* <Image fluid={data.card} style={{ width: '200px', margin: '0 auto', marginTop: '10px' }} /> */}
+              {/* <Image fluid={node.frontmatter.thumbnail} style={{ width: '200px', margin: '0 auto', marginTop: '10px' }} /> */}
               {/* <Img fluid={node.frontmatter.thumbnail} /> */}
-                <img src={node.frontmatter.thumbnail} style={{ width: '100%', heigth: '200px', margin: '0 auto', marginTop: '10px' }} alt={node.frontmatter.title}/>
+                <img src={node.frontmatter.thumbnail} style={{ width: '100%', maxHeigth: '300px', margin: '0 auto', marginTop: '10px' }} alt={node.frontmatter.title}/>
                 <h3>{node.frontmatter.title}</h3>
                 <p>Author: {node.frontmatter.author}</p>
                 <p>Price: ${node.frontmatter.price}</p>
-                <p style={{ fontSize: '11px' }}>desc: {node.frontmatter.desc}</p>
+                <p style={{ fontSize: '11px' }}>desc: {node.frontmatter.desc_}</p>
                 <button
                   type="submit"
                   className="snipcart-add-item"
